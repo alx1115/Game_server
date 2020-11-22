efad5b57bef64153349a8a1f4d656ab1202169eb
IllyaOnishchenko <illyaonishchenko2@gmail.com>
2020-11-22 14:46:01 +0200

    new buttons and other

--- Game_server/mainmenu/models.py
+++ Game_server/mainmenu/models.py
@@ -6,8 +6,3 @@ class Mainmenu(models.Model):
     pasword = models.CharField('Pasword', max_length=20)
     max_result_snake = models.IntegerField('Max_result_snake')
 
-<<<<<<< HEAD
-=======
-    def __str__(self):
-        return self.login
->>>>>>> f25b6f7bf7925e236c5e3f5fe94a751b6a019f75
