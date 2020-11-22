40564a2937b60a24e457c54fe30e4fe6894d74d9
alx1115 <klyaznikalexa2003@gmail.com>
2020-11-22 15:43:15 +0200

    html-menu-upgrade

--- Game_server/mainmenu/models.py
+++ Game_server/mainmenu/models.py
@@ -6,8 +6,5 @@ class Mainmenu(models.Model):
     pasword = models.CharField('Pasword', max_length=20)
     max_result_snake = models.IntegerField('Max_result_snake')
 
-<<<<<<< HEAD
     def __str__(self):
         return self.login
-=======
->>>>>>> efad5b57bef64153349a8a1f4d656ab1202169eb
